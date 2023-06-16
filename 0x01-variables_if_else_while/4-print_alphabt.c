afb579c0656584948235f785283f58ed0c7dae8e
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-16 13:31:17 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-        char ch = 'a';
-
-        while (ch <= 'z')
-        {
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
 		if (ch == 'q' || ch == 'e')
 		{
 			ch++;
@@ -17,7 +17,7 @@ int main(void)
 		}
 		putchar(ch);
 		ch++;
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
