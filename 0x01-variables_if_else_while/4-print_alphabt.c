7694678a078b5d1c2b132c11479434ae1f293319
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-16 13:26:10 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print from a to z except q and e
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+		if (ch == 'q' || ch == 'e')
+			continue;
+
+		putchar(ch);
+		ch++;
+        }
+        putchar('\n');
+        return (0);
+}
