8bbd5d85dfcb53b00491fe81e204ab912f253cc5
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-20 08:34:50 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - print putchar
- * Return(0)
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
-	return(0);
+	printf ("_putchar\n");
+	return (0);
 }
