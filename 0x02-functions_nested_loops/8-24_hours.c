cf48f90c82447317d0408cba0f2cc79c84a70249
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-20 13:22:50 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * h = hour, m = minutes
+ * Return: 24 hour clock line by line
  */
 void jack_bauer(void)
 {
