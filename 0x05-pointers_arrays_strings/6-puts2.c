f1eb5cce39faf9e324c87d89d87bebe0e8173867
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-26 11:31:28 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,11 +5,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0 , j;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		i++;
+	}
+	for (j = 0;j <= i ; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
