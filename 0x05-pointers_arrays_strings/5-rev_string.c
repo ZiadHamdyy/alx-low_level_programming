b1f224a37641553fb5eb439a4b3e8888dea8c975
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-26 11:01:54 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _len - length of string.
+ * @i;the string.
+ * Return:the length.
+ */
+int _len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * rev_string - reverses a string.
+ * @s:a string.
+ */
+void rev_string(char *s)
+{
+	int i = _len(s), j = 0, last = i - 1;
+	char a;
+
+	while (j < i/2)
+	{
+		a = s[j];
+		s[j] = s[last];
+		s[last] = a;
+		j++;
+		last--;
+	}
+}
