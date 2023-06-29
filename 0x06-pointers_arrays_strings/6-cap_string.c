358526f3a9fc901e3a6b0535438ebf4219920ab8
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-29 15:37:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * @str:the string.
  * Return:the capitalizes.
  */
-char* cap_string(char* str)
+char *cap_string(char *str)
 {
 	int i = 0;
 
@@ -12,13 +12,13 @@ char* cap_string(char* str)
 	{
 		if (str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 		str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-		str[i] == ')' || str[i] == '{' || str[i] == '}' ||str[i] == '\t' ||
+		str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '\t' ||
 		str[i] == '\n')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
 		}
-			 
+
 		i++;
 	}
 	return (str);
