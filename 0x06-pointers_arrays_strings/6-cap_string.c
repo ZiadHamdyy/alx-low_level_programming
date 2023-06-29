28fb6b0baa859333d8c13cba43f02f762b820c82
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-06-29 15:45:21 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,6 +15,8 @@ char *cap_string(char *str)
 		str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '\t' ||
 		str[i] == '\n')
 		{
+			if (str[i] == '\t')
+				str[i] = ' ';
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
 		}
