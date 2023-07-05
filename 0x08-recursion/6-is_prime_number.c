ad20753fedc519bc669292da83bfb56230ede1b7
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-07-05 02:02:27 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,28 @@
 #include "main.h"
 /**
+ * is_prime - a supfunction to find the prime numbers.
+ * @n:is a prime number.
+ * @i:is a number.
+ * Return: 0 if its less then 2.
+ * and 1 if its prime.
+ */
+int is_prime(int n, int i)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime(n, i + 1));
+}
+/**
  * is_prime_number - returns 1 if the input integer is a prime number
  * otherwise return 0.
  * @n:a number.
@@ -8,18 +31,5 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n < 2)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_prime(n, 2));
 }
