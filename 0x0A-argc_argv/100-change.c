0003ec445c71891f20d36c6f2b9f7c8c5f79af76
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-07-07 14:48:59 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,14 +8,34 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, leastcents = 0, money = atoi(argv[]);
-	int cents[] = {25, 10, 5, 2, 1};
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		for (i = 0; i < 5; i++)
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			if 
+			result++;
+			num -= coins[j];
 		}
 	}
+
+	printf("%d\n", result);
+	return (0);
 }
+
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,21 +7,36 @@
  * Return:always return 0.
  */
 int main(int argc, char *argv[])
-{
-	int sum = 0;
-	char *c;
 
-	while (--argc)
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		for (c = argv[argc]; *c; c++)
+		if (check_num(argv[count]))
+
 		{
-			if (*c < '0' || *c > '9')
-			{
-				return (printf("Error\n"), 1);
-			}
-			sum += atoi(argv[argc]);
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
+
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
-	printf("%d\n", sum);
+
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
+
