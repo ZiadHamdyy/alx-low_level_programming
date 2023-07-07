9d8a3ab912c78000e1098d00917fd7658f4bc8bd
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-07-07 14:55:11 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * main - prints the minimum number of coins to make change for an
- * amount of money.
- * @argc:number of command line argument.
- * @argv:pointer to an array of command line argument.
- * Return:always return 0.
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
@@ -40,3 +42,4 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 	return (0);
 }
+
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,11 +3,38 @@
 #include <ctype.h>
 #include <string.h>
 /**
- * main - adds positive numbers.
- * @argc:number of command line argument.
- * @argv:pointer to an array of command line argument.
- * Return:always return 0.
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
  */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 
 {
@@ -41,4 +68,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
