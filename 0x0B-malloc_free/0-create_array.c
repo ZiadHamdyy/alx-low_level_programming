9f8cffc7f4f5a8bd09ff8699bac27bed87e12b02
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-07-10 10:53:10 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
+
 	if (size == 0 || n == 0)
 		return (0);
 	while (size)
@@ -19,6 +20,6 @@ char *create_array(unsigned int size, char c)
 		size--;
 		n[size] = c;
 	}
-		
+
 	return (n);
 }
