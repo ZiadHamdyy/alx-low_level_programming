399a597466a3ee7cb06ce2b93134d7f4d87e0311
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-07-13 12:45:09 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,11 +27,14 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr = malloc(nmemb * size);
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	if (ptr == 0)
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
 		return (NULL);
 	_memset(ptr, 0, nmemb * size);
 	return (ptr);
