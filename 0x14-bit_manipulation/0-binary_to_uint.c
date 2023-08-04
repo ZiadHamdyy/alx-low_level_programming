a246f63b3749526be105575223ae86a36a2a2ac4
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-08-03 17:33:05 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0;
 	int i = 0;
 
-	for (i = 0;b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0')
 			sum = sum << 1;
