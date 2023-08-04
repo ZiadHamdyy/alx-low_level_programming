92b0eb5780456331299cb9c9eb0413717b6464c2
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-08-03 18:14:25 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,11 +16,16 @@ unsigned int count_set_bits(unsigned long int num)
 	return (count);
 }
 /**
+ * flip_bits -  returns the number of bits you would need to flip to get
+ * from one number to another.
+ * @n:a number.
+ * @m:a number.
+ * Return:returns the number of bits.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
 	unsigned int flipped_bits = count_set_bits(xor_result);
 
-	return flipped_bits;
+	return (flipped_bits);
 }
