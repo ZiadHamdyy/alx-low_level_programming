73fca5a1acc148d6b422db3b54d8080e6a29ab68
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-08-07 08:43:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = creat(filename, O_WRONLY | O_TRUNC | O_CREAT);
+	file = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	wb = write(file, text_content, strlen(text_content));
 	if (wb == -1)
 	{
