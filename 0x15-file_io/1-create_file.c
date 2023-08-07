adf64c1bd7eb2a0b27b3b28e4c28009b524957c0
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-08-07 08:35:12 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file -  creates a file.
+ * @filename:file name.
+ * @text_content:the content.
+ * Return:1 on success, -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t wb;
+
+	if (!filename)
+		return (-1);
+	file = creat(filename, O_WRONLY | O_TRUNC | O_CREAT);
+	if (file == -1)
+		return (-1);
+	wb = write(file, text_content, strlen(text_content));
+	if (wb == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+
+}
