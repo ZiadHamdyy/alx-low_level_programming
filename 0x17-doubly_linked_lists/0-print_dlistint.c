e24b834056459f77404eafe51290cb95d9a17c1d
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-14 07:38:33 -0700

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - ..........
+ * @h:the header.
+ * Return:the number of the nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	int count = 0;
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
