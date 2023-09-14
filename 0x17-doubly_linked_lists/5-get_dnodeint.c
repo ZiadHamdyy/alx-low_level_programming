40687b8886ddde31f91feeecd87b11f9e71f9919
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-14 10:19:35 -0700

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,12 +10,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *ptr = head;
 	unsigned int i = 0;
 
-	if (!ptr)
+	if (index < i)
 		return (NULL);
 	while (i < index)
 	{
 		ptr = ptr->next;
 		i++;
 	}
+	if (index > i)
+		return (NULL);
 	return (ptr);
 }
