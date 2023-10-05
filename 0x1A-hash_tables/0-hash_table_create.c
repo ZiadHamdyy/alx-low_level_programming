0b1a877992e86ef62cc14e7d56b70ebc1ffecb9b
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-05 04:35:53 -0700

    task 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,8 @@
 #include "hash_tables.h"
 /**
+ * hash_table_create - .........
+ * @size:size of the array.
+ * Return:pointer to hash taple
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - ......
+ * @key:the key.
+ * @size:size of the array.
+ * Return:the index at which the key/value
+ * pair should be stored in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key);
+
+	return (unsigned long int)(index % size);
+}
