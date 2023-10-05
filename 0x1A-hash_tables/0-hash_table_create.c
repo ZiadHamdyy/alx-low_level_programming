103fc6bfd070f85e79227b3874c53d8b92fdd0b3
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-05 08:11:45 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,8 +8,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
 
-	ptr = malloc(sizeof(hash_node_t *) * size);
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
 		return (NULL);
+
+	ptr->array = malloc(sizeof(hash_node_t *) * size);
+	if (ptr->array == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	return (ptr);
 }
