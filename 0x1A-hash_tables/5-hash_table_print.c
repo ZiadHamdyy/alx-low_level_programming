2376741a1ad223e5f903bf89f87753dd8378e842
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-06 01:02:37 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - ...........
+ * @ht:the hash table.
+ * Return:void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int flag = 0;
+	hash_node_t *node;
+
+	while (ht->size > i)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			flag = 1;
+		}
+		i++;
+	}
+}
